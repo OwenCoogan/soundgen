c29d19197c89fb55cf26113099dc529d0fe2b758
Owen Coogan <owen.coogan@myjobglasses.com>
2021-09-21 12:20:02 +0200

    debug de cette putain de frame de webcam

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Soundgen App</h1>
+  <TensorflowWindow/>
+  <ActivateCam/>
 </template>
 
 <script>
+import TensorflowWindow from "@/components/TensorflowWindow"
+import ActivateCam from "@/components/ActivateCam"
 export default {
   name: 'App',
   components: {
+    TensorflowWindow,
+    ActivateCam
   }
 }
 </script>
---
+++ src/components/TensorflowWindow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <div id="video-container">
+    <video id="camera-stream" ref="camera-stream" width="500" controls  ></video>
+    </div>
+  </div>
+  <div>
+  <button v-on:click="toggleCamera">Activate Camera</button>
+  <button v-on:click="deactivateCamera">Deactivate Camera</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {  
+          data(){
+            return {
+              localMediaStream: []
+            }
+          },
+    
+          startVideoStream(){
+            if (navigator.getUserMedia) {
+          navigator.getUserMedia(
+          {
+            video: true
+          },
+          function(localMediaStream) {
+          console.log(localMediaStream)
+          var video = document.querySelector("#camera-stream");
+          video.srcObject = localMediaStream;  
+          },
+          function(err) {
+            console.log('The following error occurred when trying to use getUserMedia: ' + err);
+          }
+        );
+
+      }
+      
+    }
+  },
+  mounted () {
+    this.startVideoStream()
+  }
+}
+
+</script>
+
