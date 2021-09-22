a3ef3a5d07973b396bb68590985c39f7a6a83641
Owen Coogan <owen.coogan@myjobglasses.com>
2021-09-22 10:34:35 +0200

    adding music player component

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button v-on:click="createSound">Create Sound</button>
+  </div>
+</template>
+
+<script>
+import * as Tone from 'tone'
+export default {
+  methods: {
+    createSound(){
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C4", "8n");
+    }
+    
+  },
+  mounted () {
+  }
+}
+
+</script>
+
--- src/views/VideoPlayer.vue
+++ src/views/VideoPlayer.vue
@@ -8,16 +8,22 @@
       <button v-on:click="toggleCamera">Activate Camera</button>
       <button v-on:click="deactivateCamera">Deactivate Camera</button>
    </div>
+   <MusicPlayer/>
 </template>
 <script>
    import * as tf from '@tensorflow/tfjs'
    import * as handpose from '@tensorflow-models/handpose'
+   import MusicPlayer from '@/components/MusicPlayer'
    export default {
    data(){
      return {
-       localMediaStream: []
+       localMediaStream: [],
+       
      }
    },
+   components: {
+    MusicPlayer
+  },
    setup() {
    
      
