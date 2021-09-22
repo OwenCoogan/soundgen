8aa948ddc6330f12905e15f81890a02536f78fb0
StanDelamourd <sdelamourd@gmail.com>
2021-09-22 15:14:19 +0200

    commit

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -10,13 +10,25 @@ export default {
   methods: {
     createSound(){
     const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C4", "8n");
+    const notes = [
+      "C4", "E4", "G4",
+      "C5", "E5", "G5"
+    ];
+    var index=0;
+
+    Tone.Transport.scheduleRepeat(time => {
+      repeat(time);
+    }, "8n");
+    console.log(Tone.Transport.bpn.value);
+  
+    function repeat(time){
+    let note = notes[index % notes.length];
+    synth.triggerAttackRelease(note, "8n", time);
+    index++;
     }
-    
   },
   mounted () {
   }
 }
-
+}
 </script>
-
