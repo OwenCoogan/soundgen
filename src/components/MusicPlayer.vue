d9a73370ba2b723948d9168bedbd91dafaf75d68
Owen Coogan <owen.coogan@myjobglasses.com>
2021-09-23 09:20:32 +0200

    refacto synth functions

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -9,14 +9,7 @@
 import * as Tone from 'tone'
 export default {
   methods: {
-    createC4(){
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C4", "8n");
-    },
-    createB4(){
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("B4", "8n");
-    },
+    /*
     createA4(){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("A4", "8n");
@@ -24,7 +17,13 @@ export default {
     createD4(){
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("D4", "8n");
+    },*/
+
+    createSynth(note){
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(note, "8n");
     },
+
     
     emitDrumloop(){
       document.querySelector("#start-music").click()
@@ -32,15 +31,14 @@ export default {
 
     getUpdatedSound(){
     const innerWidthVideoWindow = 600/4
-    console.log(innerWidthVideoWindow)
     const innerHeightVideoWindow = 500/2
     console.log(innerHeightVideoWindow)
     this.$store.subscribe((mutation) => {
-      if(mutation.payload.bottomRight[0] > innerWidthVideoWindow && mutation.payload.bottomRight[1] < innerWidthVideoWindow*2){
-        this.createC4()
+      if(mutation.payload.bottomRight[0] > innerWidthVideoWindow){
+        this.createSynth("C4")
       }
-      if(mutation.payload.bottomRight[0] < innerWidthVideoWindow && mutation.payload.bottomRight[1] > innerWidthVideoWindow*2){
-        this.createB4()
+      if(mutation.payload.bottomRight[0] < innerWidthVideoWindow) {
+        this.createSynth("B4")
       }
       else{
         this.emitDrumloop()
